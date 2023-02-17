f764e10348a48c96defb4f55c33ca415fed53740
LuqSpeezy <spencermurithi@gmail.com>
2023-02-17 06:40:06 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,18 @@ int main(void)
 	int d;
 
 	c = 'a';
+	d = 0;
 	while
 		(d < 10)
 		{
 			putchar(d + '0');
 			d++;
+		}
+	while
+		(c <= 'f')
+		{
+			putchar(c);
+			c++;
 		}
 	putchar('\n');
 	return (0);
