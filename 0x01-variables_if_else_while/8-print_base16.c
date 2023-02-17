66553240ab7b92dab176db27de75648d20310648
LuqSpeezy <spencermurithi@gmail.com>
2023-02-17 06:32:10 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ */
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	while
+		(d < 10)
+		{
+			putchar(d + '0');
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
